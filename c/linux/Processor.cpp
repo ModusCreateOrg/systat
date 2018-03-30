cbc8eb4edff560eb13b5629c8b0dfbb8b93ba8c2
Michael Schwartz <mykesx@gmail.com>
2018-03-30 08:32:54 -0700

    formatting

--- c/linux/Processor.cpp
+++ c/linux/Processor.cpp
@@ -131,8 +131,8 @@ void Processor::update() {
 }
 
 void Processor::print() {
-  console.inverseln("%-6s %7s %7s %7s %7s %7s %7s %7s", "CPUS", "User", "System",
-                    "Nice", "Idle", "IOWait", "IRQ", "SoftIRQ");
+  console.inverseln("%-6s %7s %7s %7s %7s %7s %7s %7s", "CPUS", "User",
+                    "System", "Nice", "Idle", "IOWait", "IRQ", "SoftIRQ");
 
 #if 0
     printf("LAST\n");
